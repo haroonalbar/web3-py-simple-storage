165ee93f34942b6f657fb88b9c651ed635a02249
haroon <91939783+haroonalbar@users.noreply.github.com>
2022-03-15 21:27:15 +0530

    ok

--- deploy.py
+++ deploy.py
@@ -1,6 +1,14 @@
-from solcx compile 
+# import solcx
+from solcx import compile_standard
 
-#using simplestorage in python
+# using simplestorage in python
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
     print(simple_storage_file)
+
+# compile solidity
+compiled_sol = compile_standard(
+    "language": "Solidity",
+    "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+    
+)
